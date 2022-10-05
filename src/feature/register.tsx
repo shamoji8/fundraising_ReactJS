8883f5f075c5ea08238b21f9f0397608b07dc70f
shamoji8 <hn.11235.natu@gmail.com>
2022-10-05 17:29:32 +0900

    first commit

--- src/feature/register.tsx
+++ src/feature/register.tsx
@@ -1,11 +1,12 @@
 import * as React from "react";
+import { useState } from "react";
 import { getApi } from "../api/config/utils";
 import { useSubstrate } from "../api/providers/connectContext";
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
-import {web3FromAddress } from '@polkadot/extension-dapp';
+import { web3FromAddress } from '@polkadot/extension-dapp';
 
-export interface IRegisterProps {}
+export interface IRegisterProps { }
 
 export default function Register(props: IRegisterProps) {
   const { getExtension, accounts } = useSubstrate();
@@ -16,7 +17,7 @@ export default function Register(props: IRegisterProps) {
 
     setApiBC(api);
   };
-  
+
 
 
   React.useEffect(() => {
@@ -24,62 +25,86 @@ export default function Register(props: IRegisterProps) {
     getExtension();
   }, []);
 
+  const [text, setText] = useState("");
+
+  /* ↓state変数「addText」を定義 */
+  const [addText, setAddText] = useState("");
+
   const handleTransaction = async () => {
-    console.log("Call api");
-    console.log("Current account:{}", accounts);
-    if (accounts !== null ) {
+
+    if (accounts !== null) {
       console.log("current Account:", accounts);
       const injector = await web3FromAddress(accounts[0].address);
-    const events = new Promise(async (resolve, reject) => {
-      //ordered param
-      await apiBC.tx.templateModule
-      // fixed value
-      // dynamic value
-        .doSomething(10)
-        .signAndSend(
-          accounts[0].address,
-          { signer: injector?.signer },
-          ({ status, events, dispatchError }: any) => {
-            if (dispatchError) {
-              if (dispatchError.isModule) {
-                // for module errors, we have the section indexed, lookup
-                const decoded = apiBC.registry.findMetaError(dispatchError.asModule);
-                const { docs, name, section } = decoded;
-                const res = 'Error'.concat(':', section, '.', name);
-                //console.log(`${section}.${name}: ${docs.join(' ')}`);
-                resolve(res);
-              } else {
-                // Other, CannotLookup, BadOrigin, no extra info
-                //console.log(dispatchError.toString());
-                resolve(dispatchError.toString());
-              }
-            } else {
-              events.forEach(({ event, phase }: any) => {
-                const { data, method, section } = event;
-                //console.log('\t', phase.toString(), `: ${section}.${method}`, data.toString());
-                if (section == 'templateModule') {
-                  const res = 'Success'.concat(':', section, '.', method);
+      const events = new Promise(async (resolve, reject) => {
+
+        const num = Number(addText);
+        //ordered param
+        // change module+
+        await apiBC.tx.templateModule
+          // fixed value
+          // dynamic value
+          .doSomething(num)
+          .signAndSend(
+            accounts[0].address,
+            { signer: injector?.signer },
+            ({ status, events, dispatchError }: any) => {
+              if (dispatchError) {
+                if (dispatchError.isModule) {
+                  // for module errors, we have the section indexed, lookup
+                  const decoded = apiBC.registry.findMetaError(dispatchError.asModule);
+                  const { docs, name, section } = decoded;
+                  const res = 'Error'.concat(':', section, '.', name);
+                  //console.log(`${section}.${name}: ${docs.join(' ')}`);
                   resolve(res);
+                } else {
+                  // Other, CannotLookup, BadOrigin, no extra info
+                  //console.log(dispatchError.toString());
+                  resolve(dispatchError.toString());
                 }
-              });
+              } else {
+                events.forEach(({ event, phase }: any) => {
+                  const { data, method, section } = event;
+                  //console.log('\t', phase.toString(), `: ${section}.${method}`, data.toString());
+                  if (section == 'templateModule') {
+                    const res = 'Success'.concat(':', section, '.', method);
+                    resolve(res);
+                  }
+                });
+              }
             }
-          }
-        );
-    });
-    console.log(await events);
-  }
-  
+          );
+      });
+      console.log(await events);
+    }
+
   }
 
-  const handleQuery = async () =>{
-
+  const handleQuery = async () => {
+    // change module
     const res = await apiBC.query.templateModule.something();
     console.log(res.toHuman());
 
   }
 
+  const onClickAddText = () => {
+    setAddText(text);
+    setText("");
+  }
 
   return <div>
+    <input
+      value={text}
+      onChange={(event) => setText(event.target.value)}
+    />
+
+    {/* ↓buttonを追加 */}
+    <button onClick={onClickAddText}>追加</button>
+
+    <p>リアルタイム：{text}</p>
+
+    {/* ↓pタグを追加 */}
+    <p>ボタンクリック：{addText}</p>
+
     <Button onClick={handleTransaction}>
       Transaction me
     </Button>
